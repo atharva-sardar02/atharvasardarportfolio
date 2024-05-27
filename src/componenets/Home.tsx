1d48de0cdedc2a06fbdb4be5395e24d82a284da2
atharva-sardar02 <atharva.sardar02@gmail.com>
2024-05-27 05:15:47 -0400

    Initial commit

---
+++ src/componenets/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Home() {
+    return (
+        <div>
+            <p>
+                Hey Atharva Speaking from home here!!
+            </p>
+        </div>
+    )
+}
+
+export default Home;
