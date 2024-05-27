3514be15c020a0ee1a904094e49cb961b419fa86
atharva-sardar02 <atharva.sardar02@gmail.com>
2024-05-27 06:11:43 -0400

    Resolve auto deployment

--- src/componenets/Home.tsx
+++ src/componenets/Home.tsx
@@ -6,6 +6,7 @@ function Home() {
         <div>
             <p>
                 Hey Atharva Speaking from home here!!
+                Trying to resolve the issue with the autodeployment
             </p>
         </div>
     )
